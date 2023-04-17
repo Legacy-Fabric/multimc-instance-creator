2f844600d61128d2427a0b428096c2da88898060
Luke Schwager <luke@blucobalt.dev>
2023-04-17 08:36:50 -0700

    fix pre 1.7 versions and add attribution

--- generate.py
+++ generate.py
@@ -1,6 +1,5 @@
 import os
 import zipfile
-import pathlib
 
 loader: str = "0.14.19"
 lwjgl3: str = "3.1.6"
@@ -43,9 +42,14 @@ class Generator:
                 t.write(self.process(f.read()))
 
         mkdir_if_not_exists("temp/patches")
-        with open("skel/patches/net.fabricmc.intermediary.json", "r") as f:
-            with open("temp/patches/net.fabricmc.intermediary.json", "w") as t:
-                t.write(self.process(f.read()))
+        if self.minecraft_version == "1.6.4" or "1.5.2" or "1.4.7" or "1.3.2":
+            with open("skel/patches/net.fabricmc.intermediary.pre-1.7.json", "r") as f:
+                with open("temp/patches/net.fabricmc.intermediary.json", "w") as t:
+                    t.write(self.process(f.read()))
+        else:
+            with open("skel/patches/net.fabricmc.intermediary.json", "r") as f:
+                with open("temp/patches/net.fabricmc.intermediary.json", "w") as t:
+                    t.write(self.process(f.read()))
 
         with open("skel/legacyfabric.png", "rb") as f:
             with open("temp/legacyfabric.png", "wb") as t:
---
+++ readme.md
@@ -0,0 +1,5 @@
+# multimc-instance-creator
+generates multimc instances for legacyfabric given a loader version
+
+## thanks
+[GrayRay75](https://github.com/Grayray75) and their original [instances](https://github.com/Grayray75/LegacyFabric-MultiMC)
